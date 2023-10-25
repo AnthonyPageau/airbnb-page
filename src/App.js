4d52086456f6c5acc4bdd55c04d7a69f0cd68657
Anthony Pageau <antopageau@hotmail.com>
2023-10-25 12:24:39 -0400

    Add props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Card from './components/Card';
+import katie from "./images/katie-zaferes.png"
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Card 
+        img= {katie}
+        rating= {5.0}
+        reviewCount= "(6)"
+        country= "USA"
+        title= "Life Lessons with katie Zaferes"
+        price= {136}
+      />
     </div>
   );
 }
